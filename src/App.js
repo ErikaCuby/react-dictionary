0669f085c45d0616486220b1423ce364de5857a2
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-05-14 00:03:27 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Dictionary app in progress...</h1>
+        <Dictionary />
       </header>
     </div>
   );
