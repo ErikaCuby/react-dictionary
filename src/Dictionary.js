f946f0018b1b96eceadde0ae01dc7bc4d2d69c83
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-05-14 20:05:09 +0200

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
 
-    function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition...`);
-    }
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
-    return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
-            </form>
-        </div>
-    );
+  function search(event) {
+    event.preventDefault();
+    let apiKey = "b5913f071fao57fb23b245a065fb8tac";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
