9710ab787120cbb615ca77f93ed4a2e85f73ccbf
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-05-28 01:00:26 +0200

    Added footer and created Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
+
+  function handleImageResponse(response) {
+    setPhotos(response.data);
+    console.log(response.data);
+  }
 
   function handleResponse(response) {
     setResults(response.data);
+    let apiKey = "b5913f071fao57fb23b245a065fb8tac";
+    let apiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleImageResponse);
   }
 
   function search() {
@@ -50,6 +60,25 @@ export default function Dictionary(props) {
           <p className="hint">e.g. kindness, sunset, wine, love...</p>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/ErikaCuby"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Erika Cuby
+          </a>{" "}
+          and it is open-sourced on{" "}
+          <a
+            href="https://github.com/ErikaCuby/react-dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return "Hi from photos"
+  }}
