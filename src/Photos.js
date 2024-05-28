591dd3555a2112027041a9ea1a88980494b7e149
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-05-28 20:52:06 +0200

    Added flexbox into the gallery instead of bootstrap

--- src/Photos.js
+++ src/Photos.js
@@ -5,10 +5,10 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div className="row mt-3 mb-1">
+        <div className="gallery-container">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="image" key={index}>
                 <a
                   href={photo.src.original}
                   target="_blank"
